4b3406d2aba22e13ac812be9caaec9a3d287f270
Ubuntu <ubuntu@ip-172-31-12-63.ec2.internal>
2021-09-13 23:13:50 +0000

    done

---
+++ area_and_perimeter_of_a_circle.py
@@ -0,0 +1,23 @@
+# !/user/bin/env python3
+
+# Created by Trent Hodgins
+# Created on 09/13/20
+# This program calculates the area and perimeter of a rectangle
+# with dimensions 5cm x 3cm
+
+import math
+
+
+def main():
+    # this function calculates the area and perimeter
+
+    print("If a circle has a radius of 15mm: ")
+    print("")
+    print("Area is {}mm2.".format(math.pi * 15 ** 2))
+    print("Perimeter is {}mm.".format(2 * math.pi * 15))
+    print("")
+    print("Done.")
+
+
+if __name__ == "__main__":
+    main()
